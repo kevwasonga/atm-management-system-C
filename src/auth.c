854afee39b375e5b65b5a39f91d6d023aeb2e645
kevwasonga <kevinwasonga116@gmail.com>
2025-05-12 05:52:15 +0300

    Implement user login and registration system with password hiding and file storage
    - Added  to allow user login with password input hidden.
    - Created  to fetch stored password from file based on username.
    - Introduced  to generate unique user IDs based on the highest existing ID in the file.
    - Implemented  to check if a username is already registered.
    - Developed  to handle user registration with username and hidden password input.
    - Users are stored in a file () with their ID, username, and password.

--- src/auth.c
+++ src/auth.c
@@ -44,7 +44,7 @@ const char *getPassword(struct User u)
         exit(1);
     }
 
-    while (fscanf(fp, "%s %s", userChecker.name, userChecker.password) != EOF)
+    while (fscanf(fp, "%d %s %s", &userChecker.id, userChecker.name, userChecker.password) != EOF)
     {
         if (strcmp(userChecker.name, u.name) == 0)
         {
@@ -56,4 +56,112 @@ const char *getPassword(struct User u)
 
     fclose(fp);
     return "no user found";
+}
+
+int getUserId()
+{
+    FILE *fp;
+    struct User userChecker;
+    int maxId = -1;
+
+    if ((fp = fopen("./data/users.txt", "r")) == NULL)
+    {
+        return 0; // First user will have ID 0
+    }
+
+    while (fscanf(fp, "%d %s %s", &userChecker.id, userChecker.name, userChecker.password) != EOF)
+    {
+        if (userChecker.id > maxId)
+        {
+            maxId = userChecker.id;
+        }
+    }
+
+    fclose(fp);
+    return maxId + 1; // Return next available ID
+}
+
+int isUsernameTaken(char username[50])
+{
+    FILE *fp;
+    struct User userChecker;
+
+    if ((fp = fopen("./data/users.txt", "r")) == NULL)
+    {
+        return 0; // No users yet, so username is not taken
+    }
+
+    while (fscanf(fp, "%d %s %s", &userChecker.id, userChecker.name, userChecker.password) != EOF)
+    {
+        if (strcmp(userChecker.name, username) == 0)
+        {
+            fclose(fp);
+            return 1; // Username is taken
+        }
+    }
+
+    fclose(fp);
+    return 0; // Username is not taken
+}
+
+void registerMenu(char name[50], char password[50])
+{
+    struct termios oflags, nflags;
+    FILE *fp;
+
+    system("clear");
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Registration:");
+
+    // Get username
+    while (1) {
+        printf("\n\n\t\t\t\tEnter username: ");
+        scanf("%s", name);
+
+        // Check if username is already taken
+        if (isUsernameTaken(name)) {
+            printf("\n\t\t\t\tUsername already taken. Please choose another.");
+        } else {
+            break;
+        }
+    }
+
+    // Get password with hidden input
+    tcgetattr(fileno(stdin), &oflags);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
+
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
+    {
+        perror("tcsetattr");
+        return exit(1);
+    }
+
+    printf("\n\n\t\t\t\tEnter password: ");
+    scanf("%s", password);
+
+    // Restore terminal
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+    {
+        perror("tcsetattr");
+        return exit(1);
+    }
+
+    // Save user to file
+    int userId = getUserId();
+
+    if ((fp = fopen("./data/users.txt", "a")) == NULL)
+    {
+        printf("Error! opening file");
+        exit(1);
+    }
+
+    fprintf(fp, "%d %s %s\n", userId, name, password);
+    fclose(fp);
+
+    printf("\n\n\t\t\t\tRegistration successful!");
+    printf("\n\n\t\t\t\tYour user ID is: %d", userId);
+    printf("\n\n\t\t\t\tPress any key to continue...");
+    getchar(); // Consume newline
+    getchar(); // Wait for keypress
 }
